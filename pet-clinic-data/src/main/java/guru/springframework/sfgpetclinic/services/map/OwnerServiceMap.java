a1076a648ea611e263325c46849e7cfebaeeb183
chandre <disable1992@gmail.com>
2021-04-18 22:04:26 +0300

    Added Pets and Contact info to owners on startup. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,18 +1,46 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Owner findByLastName(String lastName) {
         return this.findAll().stream()
                 .filter(owner -> Objects.equals(lastName, owner.getLastName()))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() == null) {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    pet.setPetType(petTypeService.save(pet.getPetType()));
+                    pet.setId(petService.save(pet).getId());
+                });
+            }
+        }
+        return super.save(object);
     }
 }
